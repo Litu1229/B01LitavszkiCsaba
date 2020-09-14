93ab5f383f77a71a8ee07a7b1ed12a71a1d27fb7
Ripperoni <lcsabi9d@gmail.com>
2020-09-14 10:54:17 +0200

    Add project files.

---
+++ B01LitavszkiCsaba/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace B01LitavszkiCsaba
+{
+    class Program
+    {
+        static void beolvasas()
+        {
+            StreamReader sr = new StreamReader("adatok.dat");
+        }
+        static void Main(string[] args)
+        {
+            beolvasas();
+        }
+    }
+}
